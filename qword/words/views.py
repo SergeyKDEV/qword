fa6dd73062753948ef3c38364cfeb46bfe1dd32c
Sergey Kulbida <kulbida.sergey@outlook.com>
2024-02-18 15:18:06 +0700

    Add WordCreateCBV

--- qword/words/views.py
+++ qword/words/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import get_object_or_404, redirect, render
-from django.views.generic import ListView
+from django.views.generic import CreateView, ListView
 
 from .forms import WordForm
 from .models import Word
@@ -11,17 +11,10 @@ class WordListView(ListView):
     template_name = 'words/word_list.html'
 
 
-def words_add(request):
+class WordCreateView(CreateView):
     """Добавляет слово в БД."""
-    template_name = 'words/word_add.html'
-    form = WordForm(request.POST or None)
-    context = {
-        'form': form
-    }
-    if form.is_valid():
-        form.save()
-        return redirect('words:list')
-    return render(request, template_name, context)
+    model = Word
+    fields = ('word', 'translate')
 
 
 def words_delete(request, pk):
